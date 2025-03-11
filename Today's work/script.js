8e02bc9d0723be4ac3e837317bfb32b014c52c1e
Ahum Maitra <theahummmaitra+git@gmail.com>
2025-03-12 02:32:24 +0530

    Created second project but still building, sorry for lateness

--- Today's work/script.js
+++ Today's work/script.js
@@ -1,9 +1,6 @@
-function navbutton() {
-    Magic= prompt("Thanks (Enter secret code)");
-    if (Magic=="Wingardium Leviosa") {
-        document.getElementById("navb").innerHTML= "Magic just happen";
-    }
-    else{
-        alert("Click the button again (Wrong code)")
+function hover() {
+    var a = document.getElementsByTagName('a');
+    if (a == hover) {
+        document.;
     }
 }
